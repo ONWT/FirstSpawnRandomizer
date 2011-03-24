2e8bed8bd2e6325ca8fe6c3308522e8d201239e7
Tom Träskbäck <ttraskback@gmail.com>
2011-03-24 10:03:46 +0200

    Added Command to teleport you to random spawn and option to turn off first random spawn. /random

--- src/rocode/FirstSpawnRandomizer/FirstSpawnRandomizer.java
+++ src/rocode/FirstSpawnRandomizer/FirstSpawnRandomizer.java
@@ -1,9 +1,17 @@
 package rocode.FirstSpawnRandomizer;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
+import java.util.Scanner;
+
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
@@ -16,12 +24,16 @@ import org.bukkit.plugin.PluginManager;
  * @author rocode
  */
 public class FirstSpawnRandomizer extends JavaPlugin {
+	private static final String filePath = "Spawn.properties";
 	private final FirstSpawnRandomizerPlayerListener playerListener = new FirstSpawnRandomizerPlayerListener(this);
 	private static Location mainLoc;
+	public boolean RandomSpawn=false;
+	private Integer delay=200;
 
     private Random rand = new Random(System.nanoTime());
 
     public void onEnable() {
+    	loadProperties();
     	setMainLoc(new Location(this.getServer().getWorlds().get(0), 0, 0, 0));
     	PluginManager pm = getServer().getPluginManager();
     	pm.registerEvent(Type.PLAYER_JOIN, playerListener, Priority.Low, this);
@@ -29,16 +41,71 @@ public class FirstSpawnRandomizer extends JavaPlugin {
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
         for(World world:this.getServer().getWorlds())
         {
-        	getServer().getScheduler().scheduleAsyncRepeatingTask(this,new FirstSpawnRandomizerLocationRand(this,world),1L, 200L);
+        	getServer().getScheduler().scheduleAsyncRepeatingTask(this,new FirstSpawnRandomizerLocationRand(this,world),1L, delay);
         }
     }
     public void onDisable() {
         System.out.println("FirstSpawnRandomizer Disabled!");
     }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args)
+	{
+		String commandName = command.getName().toLowerCase();
+		if(commandName=="random")
+		{
+			if(sender instanceof Player)
+			{
+				this.teleport((Player) sender);
+				return true;
+			}else{
+				for(Player p:sender.getServer().getOnlinePlayers())
+				{
+					this.teleport(p);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+    
 	protected void teleport(Player player) {
 		Location location = getRandomLocation(getMainLoc(),40);
 		player.teleportTo(location);
 	}
+	private void loadProperties() {
+        Scanner properties;
+        String next;
+        
+        getDataFolder().mkdir();
+        try {
+            properties = new Scanner(new File(getDataFolder().getPath()+"/"+filePath));
+            
+            while(properties.hasNext()) {
+                next = properties.next();
+				if(next.equals("RandomFirstSpawn:"))
+                    RandomSpawn = properties.nextBoolean();
+				if(next.equals("DelayBetweanSpawnChange:"))
+                    delay = properties.nextInt();
+            }
+            
+            properties.close();
+        } catch (FileNotFoundException fnfe) {
+            saveProperties();
+            return;
+        }
+    }
+    private void saveProperties() {
+        BufferedWriter file = null;
+        
+        try {
+            file = new BufferedWriter(new FileWriter(getDataFolder().getPath()+"/"+filePath));
+            file.write("RandomFirstSpawn: "+RandomSpawn);
+            file.newLine();
+            file.write("DelayBetweanSpawnChange: "+delay);
+            file.close();
+        } catch (IOException ioe) {}//do nothing
+    }
 	/**
 	 * old version
 	 * @param world
